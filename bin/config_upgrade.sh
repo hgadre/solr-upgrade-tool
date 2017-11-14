4144c6037c667955f5a0fb189f16dd7249500327
Hrishikesh Gadre <hgadre@cloudera.com>
2017-11-14 12:05:29 -0800

    Configure maven to generate .tar.gz artifact as part of the build

--- bin/config_upgrade.sh
+++ bin/config_upgrade.sh
@@ -5,5 +5,5 @@
 JVM="java"
 SCRIPT_DIR=$(dirname "$0")
 
-PATH="${JAVA_HOME}/bin:${PATH}" ${JVM} $JAVA_OPTS -jar "${SCRIPT_DIR}/../target/solr-upgrade-tool-0.0.1-SNAPSHOT.jar"  "$@"
+PATH="${JAVA_HOME}/bin:${PATH}" ${JVM} $JAVA_OPTS -cp "${SCRIPT_DIR}/lib/*" org.apache.solr.config.upgrade.ConfigUpgradeTool "$@"
 
